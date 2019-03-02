3562938991d9d3528c1aea6c4e3980efa5f91e2b
gN <inozytol@gmail.com>
2019-03-02 22:26:21 +0100

    Created empty project in idea with sample java file

---
+++ src/main/chapter4/functionalInterfaces/Combining.java
@@ -0,0 +1,9 @@
+package chapter4.chapter4.functionalInterfaces;
+
+public class Combining {
+    public static void main(String [] args){
+        System.out.println("Witaj w pierwszej klasie");
+
+    }
+
+}
