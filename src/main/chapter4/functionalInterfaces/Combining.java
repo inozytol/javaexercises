bd2c2054a743969cacbfa257eb45f4ac78b862a5
gN <inozytol@gmail.com>
2019-03-03 13:13:02 +0100

    Adding IntStream example

--- src/main/chapter4/functionalInterfaces/Combining.java
+++ src/main/chapter4/functionalInterfaces/Combining.java
@@ -1,9 +1,10 @@
 package chapter4.chapter4.functionalInterfaces;
 
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class Combining {
-    public static boolean answerPredicate(int num, Predicate<Integer> pred){
+    private static boolean answerPredicate(int num, Predicate<Integer> pred){
         return pred.test(num);
 
     }
@@ -14,6 +15,13 @@ public class Combining {
         Predicate<Integer> pred2 = i -> i > 3;
         Predicate<Integer> predCombined = pred1.and(pred2);
         System.out.println(predCombined.test(20));
+
+        IntStream generate = IntStream.iterate(1, c -> c + 4).limit(15);
+
+        System.out.println(generate.sum());
+
+        // Will cause exception as stream was closed already
+        //generate.forEach(System.out::println);
     }
 
 }
